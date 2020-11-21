93972a6f245b5b3fc4bcde0bb5da785d94298473
Fernando <lopezcesar292@gmail.com>
2020-11-21 17:40:03 -0600

    Create function to get genres and podcasts separately

--- database/database_loader.py
+++ database/database_loader.py
@@ -7,3 +7,15 @@ def __fetch_api():
     return results
 
 
+def __get_data(results):
+    genres_id = set()
+    genres = list()
+
+    for podcast in results:
+        # Extract genres from each result
+        podcast_genres = podcast.get('genres', [])
+        for genre in podcast_genres:
+            if genre['genreId'] not in genres_id:
+                genres_id.add(genre['genreId'])
+                genres.append(genre)
+    return genres, results
