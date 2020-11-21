a8d09c4440639b40c9dc458df4ea1c84a2587b80
Fernando <lopezcesar292@gmail.com>
2020-11-21 17:40:42 -0600

    Create function to populate the database

--- database/database_loader.py
+++ database/database_loader.py
@@ -19,3 +19,41 @@ def __get_data(results):
                 genres_id.add(genre['genreId'])
                 genres.append(genre)
     return genres, results
+
+
+def populate_db(db, Genre, Podcast):
+    results = __fetch_api()
+    genres, podcasts = __get_data(results)
+    genres_dict = {}
+    # Save genres on database
+    for genre in genres:
+        new_genre = Genre(genreId=int(genre['genreId']), name=genre['name'], url=genre['url'])
+        genres_dict[genre['genreId']] = new_genre
+        db.session.add(new_genre)
+        db.session.commit()
+    # Save podcasts with related genres
+    for podcast in podcasts:
+        new_podcast = Podcast(
+            id=int(podcast['id']),
+            name=podcast['name'],
+            kind=podcast['kind'],
+            copyright=podcast.get('copyright', None),
+            releaseDate=podcast['releaseDate'],
+            contentAdvisoryRating=podcast.get('contentAdvisoryRating', None),
+            url=podcast['url'],
+            artworkUrl100=podcast['artworkUrl100'],
+            artistName=podcast['artistName'],
+            artistId=podcast.get('artistId', None),
+            artistUrl=podcast.get('artistUrl', None),
+        )
+        # Save podcast
+        db.session.add(new_podcast)
+        db.session.commit()
+
+        # Add related genres to podcast
+        for genre in podcast['genres']:
+            new_podcast.genres.append(genres_dict[genre['genreId']])
+
+        db.session.commit()
+
+    print('Database has been populated!!!')
